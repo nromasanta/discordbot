8c3ebec38f0bf0e2708706bd2d0a3b99e83e40c5
Nicholas Romasanta <nichosanta14@gmail.com>
2024-10-02 17:45:08 -0700

    updated user modal

--- commands/utils/user.js
+++ commands/utils/user.js
@@ -1,13 +1,59 @@
-
-import { SlashCommandBuilder } from 'discord.js';
-
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { Users } from '../../events/ready.js';
 
 const commandData = new SlashCommandBuilder()
     .setName('user')
     .setDescription('Provides info about user');
 
 async function execute(interaction) {
-    await interaction.reply(`${interaction.user.username}. Join Date: ${interaction.member.joinedAt}`);
+    // get the user data from the db
+    console.log(`${interaction.user.username}`)
+    let userData = await Users.findAll({
+        where: {
+            name: interaction.user.username,
+        }
+    }); // returns array of obj
+
+    console.log('Userdata:', userData[0].dataValues)
+    const username = userData[0].dataValues.name;
+    const currency = userData[0].dataValues.currency;
+    const wins = userData[0].dataValues.wins;
+    const losses = userData[0].dataValues.losses;
+
+    const guild = interaction.guild;
+    const serverIcon = guild.iconURL();
+    const serverStatEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(`User Stats for ${username}`)
+        .addFields(
+            {
+                name: 'Slab Bucks:',
+                value: `${currency}`,
+                inline: false
+            },
+            {
+                name: 'Wins',
+                value: `${wins}`,
+                inline: true
+            },
+            {
+                name: 'Losses',
+                value: `${losses}`,
+                inline: true
+            },
+            {
+                name: '%',
+                value: 'tempval',
+                inline: true
+            },
+            {
+                name: 'Highest Payout',
+                value: 'tempval',
+                inline: false
+            }
+        )
+        .setThumbnail(serverIcon)
+    await interaction.reply({ embeds: [serverStatEmbed] });
 }
 
 const userCommand = {
